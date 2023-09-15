3fa410b0bd57e0ac809eadf1c773339f8635a81a
Jonathas Souza <johnnysxpjourney@gmail.com>
2023-09-14 22:59:09 -0300

    Cria a pasta 'course03' e seus respectivos conceitos e exercícios na pasta 'modulo02' do Bootcamp 'Coding the Future - GFT e AWS

---
+++ bootcamps/coding_the_future-gft_e_aws/module02/course03/class02-list/src/list/operacoes_basicas/Tarefa.java
@@ -0,0 +1,20 @@
+package list.operacoes_basicas;
+
+public class Tarefa {
+
+    // Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
